7444b933279be4ee0a3c5b649658ac5987aa8fc6
unknown <shireenkk28@gmail.com>
2021-04-16 20:57:37 +0530

    [shireen]Added AddressBook UC1

---
+++ AddressBookUsingLinq/AddressBookUsingLinq/AddressBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookUsingLinq
+{
+    class AddressBook
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int Zip { get; set; }
+        public int PhNum { get; set; }
+        public string email { get; set; }
+    }
+}
